c74ebc9f040a754b17015160a5916cd650fd99dd
chrisvasqm <christianvasquezmelo@gmail.com>
2022-09-14 14:42:50 -0400

    Create first layout

--- app/src/main/java/com/example/devexpertomovies/MainActivity.kt
+++ app/src/main/java/com/example/devexpertomovies/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.devexpertomovies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.devexpertomovies.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonShow.setOnClickListener { onShowClicked() }
     }
+
+    private fun onShowClicked() {
+        val message = binding.editMessage.text
+        if (message.isNotBlank())
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
 }
